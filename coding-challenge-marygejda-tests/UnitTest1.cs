ca0fd4c5d0b545988aeb5feaa6ed7b3cb89ceb6c
Mary Gejda <ZuRyu@hotmail.co.uk>
2018-03-26 18:17:36 +0100

    P: UnitTest1.cs A: Added a few more test cases for adding a ship R: Better test coverage

--- coding-challenge-marygejda-tests/UnitTest1.cs
+++ coding-challenge-marygejda-tests/UnitTest1.cs
@@ -8,10 +8,17 @@ namespace coding_challenge_marygejda_tests
     {
         [Theory]
         [InlineData(0, 0, Orientation.N)]
-        public void Test1(byte x, byte y, Orientation orientation)
+        [InlineData(0, 0, Orientation.S)]
+        [InlineData(0, 0, Orientation.E)]
+        [InlineData(0, 0, Orientation.W)]
+        [InlineData(51, 0, Orientation.N)]
+        [InlineData(0, 51, Orientation.N)]
+        public void CanCreateShip(byte x, byte y, Orientation orientation)
         {
             Ship ship = new Ship(x, y, orientation);
             Assert.Equal(x, ship.GetX());
+            Assert.Equal(y, ship.GetY());
+            Assert.Equal(orientation, ship.GetOrientation());
         }
     }
 }
