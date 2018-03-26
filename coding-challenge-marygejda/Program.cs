7cb328e2808fae4bb4ae49cd8c53c7ba915cf453
Mary Gejda <ZuRyu@hotmail.co.uk>
2018-03-26 15:13:54 +0100

    Initial project setup.

---
+++ coding-challenge-marygejda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge_marygejda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
