ff65e7e1f26511c290f36841254f694596e6721b
Mary Gejda <ZuRyu@hotmail.co.uk>
2018-03-26 15:26:46 +0100

    P: Ship.cs A: Added Ship class R: To hold data on the ships

---
+++ coding-challenge-marygejda/Ship.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge_marygejda
+{
+    class Ship
+    {
+        private byte x;
+        private byte y;
+        private Orientation orientation;
+
+        public Ship(byte x, byte y, Orientation orientation)
+        {
+            this.x = x;
+            this.y = y;
+            this.orientation = orientation;
+        }
+    }
+
+    public enum Orientation { N, E, S, W }
+}
